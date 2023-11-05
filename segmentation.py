1cd50127c32fc17b17464d4824e7188e1a86ab4b
Jordi <jordi8101@gmail.com>
2023-11-05 13:12:25 -0800

    fix: remove yolov5 local

--- segmentation.py
+++ segmentation.py
@@ -1,14 +1,11 @@
 import torch
-from PIL import Image
-import io
 
 
 def get_yolov5():
     try:
-        model = torch.hub.load(
-            "./yolov5", "custom", path="./model/best.pt", source="local"
-        )
+        model = torch.hub.load("ultralytics/yolov5", "custom", path="./model/best.pt")
         model.conf = 0.5
+
         return model
     except Exception as e:
         print(e)
